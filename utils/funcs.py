09d517c7854b2e429104d5127eba44e05ce3879a
SShiryaev <shiryaev_fox@mail.ru>
2023-12-14 21:21:58 +0300

    Add format_transactions func

--- utils/funcs.py
+++ utils/funcs.py
@@ -6,3 +6,11 @@ def load_transactions():
     with open(os.path.join('..', 'data', 'operations.json')) as file:
         transactions_list = json.load(file)
         return transactions_list
+
+
+def format_transactions(transactions):
+    format_transactions_list = []
+    for transaction in transactions:
+        if transaction != {} and transaction['state'] == "EXECUTED":
+            format_transactions_list.append(transaction)
+    return format_transactions_list
