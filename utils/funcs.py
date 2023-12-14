0575a88b69353476b5f2440d2ef0b9ef6633b238
SShiryaev <shiryaev_fox@mail.ru>
2023-12-14 22:58:37 +0300

    Add recent_transactions func

--- utils/funcs.py
+++ utils/funcs.py
@@ -14,3 +14,9 @@ def format_transactions(transactions):
         if transaction != {} and transaction['state'] == "EXECUTED":
             format_transactions_list.append(transaction)
     return format_transactions_list
+
+
+def recent_transactions():
+    sort_transactions_list = sorted(format_transactions(load_transactions()), key=lambda x: x['date'], reverse=True)
+    recent_transactions_list = sort_transactions_list[0:5]
+    return recent_transactions_list
