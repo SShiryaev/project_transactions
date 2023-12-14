1b0a0659a1449d84b44e23fdfa0a5bea44ff69d0
Sergey Shiryaev <shiryaev_fox@mail.ru>
2023-12-15 02:49:55 +0300

    Add docstrings. Fix KeyError with format_transactions func. Fix recent_transactions func

--- utils/funcs.py
+++ utils/funcs.py
@@ -3,20 +3,24 @@ import os
 
 
 def load_transactions():
+    """Фукнция чтения json файла"""
     with open(os.path.join('..', 'data', 'operations.json')) as file:
         transactions_list = json.load(file)
         return transactions_list
 
 
 def format_transactions(transactions):
+    """Функция форматирующая словари из json файла, возвращает словари с НЕ пустыми и
+    выполненными операциями, и с НЕ пустыми данными по отправителю"""
     format_transactions_list = []
     for transaction in transactions:
-        if transaction != {} and transaction['state'] == "EXECUTED":
+        if transaction != {} and transaction['state'] == "EXECUTED" and transaction.get('from'):
             format_transactions_list.append(transaction)
     return format_transactions_list
 
 
-def recent_transactions():
-    sort_transactions_list = sorted(format_transactions(load_transactions()), key=lambda x: x['date'], reverse=True)
-    recent_transactions_list = sort_transactions_list[0:5]
+def recent_transactions(transactions_list, len_list):
+    """Функция возвращает заданное колличество словарей, отсортированных по дате"""
+    sort_transactions_list = sorted(transactions_list, key=lambda x: x['date'], reverse=True)
+    recent_transactions_list = sort_transactions_list[0:len_list]
     return recent_transactions_list
